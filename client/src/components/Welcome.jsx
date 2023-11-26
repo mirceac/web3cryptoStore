2b47e4cf20bb19a39a33051e18f8fde38033adcf
Mircea Carasel <mirciuss@gmail.com>
2023-11-26 08:41:54 +0200

    Cleanup UI

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -19,7 +19,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     />
   );
 const Welcome = () => {
-    const { connectWallet, currentAccount, formData, sendTransaction, handleChange, isLoading } = useContext(TransactionContext);
+    const {currentAccount, formData, sendTransaction, handleChange, isLoading } = useContext(TransactionContext);
     
     const handleSubmit = (e) => {
         const {addressTo, amount, keyword, message} = formData;
@@ -33,58 +33,13 @@ const Welcome = () => {
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col mf:mr-10">
-                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-                        Send Crypto <br/> across the world
-                    </h1>
-                    <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-                        Export the crypto world. Buy and sell cryptocurrencies easily on Krypto.
-                    </p>
-                    {!currentAccount && (<button type="button" 
-                            onClick={connectWallet}
-                            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                        <p className="text-white text-base font-semibold">Connect Wallet</p>
-                    </button>)}
-                    <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-                        <div className={`rounded-tl-2xl ${commonStyles}`}>
-                            reliability
-                        </div>
-                        <div className={`${commonStyles}`}>
-                            security
-                        </div>
-                        <div className={`rounded-tr-2xl ${commonStyles}`}>
-                            ethereum
-                        </div>
-                        <div className={`rounded-bl-2xl ${commonStyles}`}>
-                            Web 3.0
-                        </div>
-                        <div className={`${commonStyles}`}>
-                            Low fees
-                        </div>
-                        <div className={`rounded-br-2xl ${commonStyles}`}>
-                            Blockchain
-                        </div>
-                    </div>
-                </div>
+
                 <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-                    <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
-                        <div className="flex justify-between flex-col w-full h-full">
-                            <div className="flex justify-between items-start">
-                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                                    <SiEthereum fontSize={21} color="#fff"/>
-                                </div>
-                                <BsInfoCircle fontSize={17} color="#fff"/>
-                            </div>
                             <div>
                                 <p className="text-white font-light text-sm"> 
                                   {shortenAddress(currentAccount)}
-                                </p>
-                                <p className="text-white font-semibold text-lg mt-1"> 
-                                  Ethereum
-                                </p>                                
+                                </p>                               
                             </div>
-                        </div>
-                    </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
                         <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
                         <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
